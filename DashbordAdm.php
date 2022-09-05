ffe05f5ca8a6d79506d821b10f9f68eb881669a6
kerby <lovincekerby99@gmail.com>
2022-09-05 16:56:06 -0300

    Adicionando css no Dashbord adm

--- DashbordAdm.php
+++ DashbordAdm.php
@@ -5,21 +5,95 @@
 
 ?>
 
+<!-- css -->
+<style>
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+
+    body {
+        /*background-color: #f9f9f9;*/
+        background-image: url(assets/fundo.jpeg);
+    }
+
+
+    #check{
+        display: none;
+    }
+
+    #icone{
+        cursor: pointer;
+        padding: 15px;
+        position: absolute;
+        z-index: 1;
+
+    }
+
+    .barra{
+        background-color: #333;
+        height: 100%;
+        width: 300px;
+        position: absolute;
+        transition: all .2s linear;
+        left:-300px;
+    }
+
+    nav{
+        width: 100%;
+        position: absolute;
+        top: 60px;
+    }
+
+    nav a{
+        text-decoration: none;
+    }
+
+    .link{
+        background-color: #494950;
+        padding: 20px;
+        font-family: 'Arial';
+        font-size: 12pt;
+        transition: all .2s linear;
+        color: #f4f4f9;
+        border-bottom: 2px solid #222;
+        opacity: 0;
+        margin-top: 200px;
+    }
+
+    .link:hover{
+        background-color: #050542;
+    }
+
+    #check:checked ~ .barra{
+        transform: translateX(300px);
+    }
+
+    #check:checked ~ .barra nav a .link{
+        opacity: 1;
+        margin-top:0;
+        transition-delay: .2s;
+    }
+
+</style>
+
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Menu lateral</title>
+        <title>Dashbord Adm</title>
   </head>
 
   <body>
-    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    
     <h1>MEGA CAPAS</h1>
 
 
     <input type="checkbox" id="check">
-    <label id="icone" for="check"><img src="menu.png"></label>
+    <label id="icone" for="check"><img src="assets/menu.png"></label>
 
     <div class="barra">
         <nav>
