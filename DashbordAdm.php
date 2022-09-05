ddb5d9cab1a2ccd7af2162a7e1f0ecf96a01e928
kerby <lovincekerby99@gmail.com>
2022-09-05 16:32:18 -0300

    Dashbord adm

---
+++ DashbordAdm.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Menu lateral</title>
+  </head>
+
+  <body>
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <h1>MEGA CAPAS</h1>
+
+
+    <input type="checkbox" id="check">
+    <label id="icone" for="check"><img src="menu.png"></label>
+
+    <div class="barra">
+        <nav>
+            <a href=""><div class="link">Home</div></a>
+            <a href=""><div class="link">Cadastrar Funcionario</div></a>
+            <a href=""><div class="link">Lista Funcionario</div></a>
+            <a href=""><div class="link">Ordem de Servicos</div></a>
+            <a href=""><div class="link">Lista de Serviços</div></a>
+            <a href=""><div class="link">Cadastrar Produtos</div></a>
+            <a href=""><div class="link">Fechamento caixa</div></a>
+
+            
+
+
+
+
+
+            
+        </nav>
+
+    </div>
+
+
+  </body>
+</html>
