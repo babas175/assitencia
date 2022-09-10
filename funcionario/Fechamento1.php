d383ab37ddd192753d56e37e260a06e59697d916
lubin sebastien lionel <56324874+babas175@users.noreply.github.com>
2022-09-10 02:50:42 -0300

    jj

--- funcionario/Fechamento1.php
+++ funcionario/Fechamento1.php
@@ -253,12 +253,12 @@ input[type=text], select, textarea {
 
     <div class="barra">
         <nav>
-            <a href="../DashbordFunc.php"><div class="link">Home</div></a>
-            <a href="../OrdemServico.php"><div class="link">Ordem de Servicos</div></a>
+            <a href="DashbordFunc.php"><div class="link">Home</div></a>
+            <a href="OrdemServico.php"><div class="link">Ordem de Servicos</div></a>
             <a href=""><div class="link">Listar de Serviços</div></a>
-            <a href="../CadastraProd.php"><div class="link">Cadastrar Produtos</div></a>
-            <a href="../Fechamento.php"><div class="link">Fechamento caixa</div></a>
-            <a href="sair.php"><div class="link">Sair</div></a>
+            <a href="CadastraProd2.php"><div class="link">Cadastrar Produtos</div></a>
+            <a href="Fechamento1.php"><div class="link">Fechamento caixa</div></a>
+            <a href="../sair.php"><div class="link">Sair</div></a>
             
             
         </nav>
