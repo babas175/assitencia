6cf90c41520be2bec4124131d06129189736a836
kerby <lovincekerby99@gmail.com>
2022-09-11 21:03:44 -0300

    está pronto

--- index.php
+++ index.php
@@ -5,11 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <h1> Bem Vindo ao Sistema Mega Capas</h1>
 </head>
 <body>
-    <h1>Mega Assistencia</h1>
-
-    <p> logo essa parte sera desenvolvido :), be patient<p>
+<div class="button">
+    <input type="submit" value="Fazer Login" name="entrar"/>
+  </div>
+    
     
 </body>
 </html>
+
+<style>
+
+    body{
+        background-image: url(assets/fundo.jpeg);
+        
+
+
+    }
+     h1{
+        text-align:center;
+        margin-top: 200px;
+
+
+        
+    }
+    .button{
+      border-radius: 20px;
+      cursor: pointer;
+      position:absolute;
+        top: 400px;
+        left: 650px;
+    }
+    input[type=submit]:hover {
+        background-color:  blue;
+        color; black;
+
+        }
+
+
+</style>   
